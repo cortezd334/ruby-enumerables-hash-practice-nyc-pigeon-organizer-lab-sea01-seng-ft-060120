1e65e7e81e9916b2569ae330cc8aa1dfc89ca85e
Danira Cortez <cortezd334@gmail.com>
2020-05-18 23:17:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,24 +2,25 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
-  
-  names = []
-  categories = []
-  descriptions = []
-  
   data.collect do |key, value|
-    categories << key
+  
+  # names = []
+  # categories = []
+  # descriptions = []
+  
+  # data.collect do |key, value|
+  #   categories << key
     
-      value.collect do |element, index|
-        #binding.pry
-        descriptions << element
-        names << index.sort
-        puts names
-      end
+  #     value.collect do |element, index|
+  #       #binding.pry
+  #       descriptions << element
+  #       names << index.sort
+  #       puts names
+  #     end
       
-      # #put names into {}
-      # puts pigeon_list[names] = categories.to_h
-      # pigeon_list[names][categories] = descriptions.to_s
-  end
+  #     # #put names into {}
+  #     # puts pigeon_list[names] = categories.to_h
+  #     # pigeon_list[names][categories] = descriptions.to_s
+  # end
 end
 
