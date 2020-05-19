5c9820852d82d6bc081b69a185a8ba99b76844d7
Danira Cortez <cortezd334@gmail.com>
2020-05-19 01:02:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,18 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   #binding.pry
-  data.each do |color_gender_lives, value|
-    value.each do |element, index|
-      index.each do |name|
+  data.each do |color_g_l, hash_value|
+    hash_value.each do |descriptions, array_of_names|
+      array_of_names.each do |name|
       name
       
       if !pigeon_list[name] # cuz this is now a truthy due to ! we can go through the code below, otherwise we don't
         pigeon_list[name] = {}
       end
-      if !pigeon_list[name][color_gender_lives]
-        pigeon_list[name][key] = []
+      if !pigeon_list[name][color_g_l]
+        pigeon_list[name][color_g_l] = []
       end
-      pigeon_list[name][key] << element.to_s
+      pigeon_list[name][color_g_l] << descriptions.to_s
       #binding.pry
     end
   end
