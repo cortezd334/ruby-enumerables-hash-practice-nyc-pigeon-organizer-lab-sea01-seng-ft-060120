4c219287b012abf62348c0ec26cdadc1cfa3cbc8
Danira Cortez <cortezd334@gmail.com>
2020-05-19 01:03:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,23 +19,5 @@ def nyc_pigeon_organizer(data)
   end
 end
 pigeon_list
-  # names = []
-  # categories = []
-  # descriptions = []
-  
-  # data.collect do |key, value|
-  #   categories << key
-    
-  #     value.collect do |element, index|
-  #       #binding.pry
-  #       descriptions << element
-  #       names << index.sort
-  #       puts names
-  #     end
-      
-  #     # #put names into {}
-  #     # puts pigeon_list[names] = categories.to_h
-  #     # pigeon_list[names][categories] = descriptions.to_s
-  # end
 end
 
